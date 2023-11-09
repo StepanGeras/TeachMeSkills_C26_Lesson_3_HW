0d74e2a5d582842abaf3f96669d4041dc229fdb7
Shifernik <m.Disai@mail.ru>
2023-11-09 10:00:14 +0300

    Second commit
    Task 3

--- src/Task3/Task_1.java
+++ src/Task3/Task_1.java
@@ -7,8 +7,14 @@ public class Task_1 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите количество элементов: ");
+        System.out.print("Введите количество элементов до 100: ");
         int c = scanner.nextInt();
+
+        while (c > 100){
+            System.out.println("Введите количество элементов до 100: ");
+            c = scanner.nextInt();
+        }
+
         long a = 0;
         long b = 1;
         System.out.print(a + " " + b + " ");
